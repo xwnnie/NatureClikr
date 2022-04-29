0d650c95ee9fa7d646566334b13386b189da89df
xwnnie <niexiaowen90@gmail.com>
2022-04-29 15:02:35 -0700

    test branch and pull request

--- backend/routes/api/photo.js
+++ backend/routes/api/photo.js
@@ -2,3 +2,4 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 
 const router = express.Router();
+//routes for uploading, deleting photos, view single photos

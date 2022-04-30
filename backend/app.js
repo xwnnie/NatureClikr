8254506a434ad072dae5135a771c897c50911291
xwnnie <niexiaowen90@gmail.com>
2022-04-29 22:20:37 -0700

    removed test routes

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-const { ValidationError } = require("sequelize");
 
 const routes = require("./routes");
 
@@ -13,31 +12,58 @@ const isProduction = environment === "production";
 
 const app = express();
 
-app.use(morgan('dev'));
+const { ValidationError } = require("sequelize");
+
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 
+//security
 if (!isProduction) {
+  // cors is for dev only for the different server for react and express servers
   app.use(cors());
 }
 
+// helmet helps set a variety of headers to better secure your app
+//allows images with URLs to render in deployment
 app.use(
-  helmet.crossOriginResourcePolicy({ 
-    policy: "cross-origin" 
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
   })
 );
 
+// Set the _csrf token and create req.csrfToken method
 app.use(
   csurf({
     cookie: {
       secure: isProduction,
       sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
+      httpOnly: true,
+    },
   })
 );
 
-app.use(routes); 
+/* 
+The csurf middleware will add a _csrf cookie that is HTTP-only
+(can't be read by JavaScript) to any server response.
+It also adds a method on all requests (req.csrfToken)
+that will be set to another cookie (XSRF-TOKEN) later on.
+These two cookies work together to provide CSRF
+(Cross-Site Request Forgery) protection for your application.
+The XSRF-TOKEN cookie value needs to be sent in the header of
+any request with all HTTP verbs besides GET.
+This header will be used to validate the _csrf cookie
+to confirm that the request comes from your site and
+not an unauthorized site.
+*/
+
+//connect all the routes
+app.use(routes);
+
+//error handling middleware
+
+//resource not found error-handler
+//catch unhandled requests and forward to error handler
 
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
@@ -47,7 +73,7 @@ app.use((_req, _res, next) => {
   next(err);
 });
 
-// Process sequelize errors
+//for catching sequelize errors and formatting them before sending the error response
 app.use((err, _req, _res, next) => {
   // check if error is a Sequelize error:
   if (err instanceof ValidationError) {
@@ -57,15 +83,16 @@ app.use((err, _req, _res, next) => {
   next(err);
 });
 
-// Error formatter
+//error formatter
+
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
   res.json({
-    title: err.title || 'Server Error',
+    title: err.title || "Server Error",
     message: err.message,
     errors: err.errors,
-    stack: isProduction ? null : err.stack
+    stack: isProduction ? null : err.stack,
   });
 });
 
